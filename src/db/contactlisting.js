6a7ad28834447dcd3bae3ae2cc2f56350194aa64
Rosemary <rosey.ly@hotmail.com>
2019-12-21 13:01:35 -0500

    Post route with async functions

---
+++ src/db/contactlisting.js
@@ -0,0 +1,44 @@
+'use strict';
+
+let util = require('util');
+let fs = require('fs');
+let path = require('path');
+
+// Create versions of `fs` methods we'll be using to return promises
+let readFile = util.promisify(fs.readFile);
+let writeFile = util.promisify(fs.writeFile);
+
+// Declare where the DB path is relative from where our `package.json` is
+let contactlisting=path.resolve('src/db/contactlisting.json'); 
+
+/**
+ *
+ */
+
+ // 
+ async function readContacts(){
+    let json= await readFile(contactlisting);
+    return JSON.parse(json)
+  }
+
+
+  async function writeContacts(contactlisting){
+    let json=JSON.stringify(contactlisting, null, 2); 
+    await writeFile(contactlisting, json);
+  }
+
+
+
+/**
+ *
+ */
+async function addItem(newInfo) {
+  let addItem=await readContacts();
+  addItem.push(newInfo)
+  await write(addItem);
+}
+
+module.exports = {
+  read: contactlisting,
+  addItem: addItem,
+}
--- src/index.js
+++ src/index.js
@@ -3,48 +3,88 @@
 let express = require('express');
 let app = express();
 let port = 3000;
+let router = express.Router();
 
 let util = require('util');
 let path = require('path');
 let fs = require('fs');
 
+// Apply middleware//
+app.use(express.json()); // Allows us to read JSON sent in `req.body`//
+
+app.use(router); // Apply our router as middleware
+
+
+
+
 let readFile = util.promisify(fs.readFile);
 let writeFile = util.promisify(fs.writeFile);
 
-let filePath=path.resolve('server/db/contactlisting.json'); 
+let contactlisting = path.resolve('src/db/contactlisting.json'); 
 
 async function readContacts(){
-  let json= await readFile(filePath);
-  return JSON.parse(json)
+  let json= await readFile(contactlisting);
+  return JSON.parse(json);
 }
 
-async function writeContacts(contactlisting){
-  let json=JSON.stringify(contactlisting); 
-  await writeContacts(filePath, json);
+
+async function writeContacts(ContactListing){
+  let json=JSON.stringify(contactlisting, null, 2); 
+  await writeFile(contactlisting, json);
 }
 
-app.post('/generalenquiryform', function(request,response){
+async function addItem(newInfo) {
+  let addItem=await readContacts();
+  addItem.push(newInfo)
+  await writeFile(addItem).catch(console.log('Error Caught'));
+}
 
-  response.status(200).send('Form submitted');
+function validateContactInfo(request, response, next) {
+  // If we get an invalid  in `req.body` we want to respond with a 400 status code
+  let dbItems = request.body;
+  if (!dbItems.name) {
+    response.status(400).send('"name" is a required field');
+  } else if (!dbItems.email) {
+    response.status(400).send('"email" is a required field');
+  } else {
+    next();
+  }
+}
+
+
+//Route to create an entry when the user submits their form.//
+
+app.post('/generalenquiryform', validateContactInfo,  async function(request,response,next){
+
+  await addItem(request.body);
+  response.status(201).status('it works!');
+  next();
 }
 );
 
-app.post ('/User' , function (request, response) {
-
-  response.status(201).send ('User Created');
+//Route to create or register a user.//
+app.post ('/User' , validateContactInfo, async function (request, response,next) {
+  await addItem(request.body)
+  response.sendStatus(201);
+  next();
 }
 );
+
+//Route to log a registered user in to create a session.//
 
 app.post('/Session' , function (request,response) {
 
-  response.status(200).send ('New Session created!');
+  response.status(200).send('New Session created!');
 }
 );
-app.get('/ContactListing' , function (request, response) {
-  
-  response.status(200).send('contacts')
-}
-);
+
+//Route to get a listing of all submissions.//
+
+app.get('/ContactListing', async function (request, response) {
+  response.status(200).json(await readContacts());
+});
+
+//default error handler// 
 app.use(function (error, request, response, next) {
   console.error(error.stack)
   response.status(500).send('Oh No! Something broke! ')
